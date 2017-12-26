604969bf947065ab5606301a9092664e92be2009
htoniv <vinothkumarrenganathan@users.noreply.github.com>
2017-12-26 17:30:05 +0530

    Update NumberCounter.js

--- dev/components/NumberCounter.js
+++ dev/components/NumberCounter.js
@@ -10,35 +10,25 @@ export default class NumberCounter extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			smallValue: props.min,
-			updates: 0,
 			min: props.min,
 			max: props.max
 		};
-		this.handleClick = this.handleClick.bind(this);
 	}
 
-	handleClick() {
-		this.interval = setInterval(() => this.update(), 500);
-    }
-
-    update() {
-        const {updates, min, max} = this.state;
-        this.setState({
-            smallValue: getRandomInt(min, max),
-            updates: updates + 1
-        });
-    }
-
-    componentDidUpdate() {
-    	clearInterval(this.interval);
+	shouldComponentUpdate(nextProps, nextState) {
+		if (nextProps !== nextState) {
+			return true;
+		} else {
+			return false;
+		}
     }
 
 	render() {
-		const {smallValue} = this.state;
-		return (<div> <h2 onClick={this.handleClick}>
+		const {min, max} = this.state;
+		const integer = getRandomInt(min, max);
+		return (<div> <h2>
 	                <AnimatedNumber style={{ transition: '0.8s ease-out', transitionProperty: 'background-color, color, opacity' }}
-	                    frameStyle={perc => ( perc === 100 ? {} : {opacity: 0.25} )} stepPrecision={0} value={smallValue} formatValue={n => `${'$ '+n}`}/>
+	                    frameStyle={perc => ( perc === 100 ? {} : {opacity: 0.25} )} stepPrecision={0} value={integer} formatValue={n => `${'$ '+n}`}/>
 	            </h2>
             </div>)
 		
